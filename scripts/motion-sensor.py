20161fee8c98dd0b5737edf1a5bbb76961322465
nerd <cptwonton@gmail.com>
2019-01-24 14:49:06 -0600

    mojtieonffrjeff sucks he is a bad coder and he smells :(

--- scripts/motion-sensor.py
+++ scripts/motion-sensor.py
@@ -2,16 +2,21 @@ from gpiozero import MotionSensor
 from gpiozero import LED
 from time import sleep
 
-led = LED(17)
-pir = MotionSensor(4);
-
-while True:
-    pir.wait_for_motion();
-    handle_motion();
-    
 def handle_motion():
     print("Motion detected!");
     led.on();
     sleep(1);
     led.off();
     sleep(1);
+
+led = LED(17)
+pir = MotionSensor(18);
+
+print(pir.pin.number);
+pir.when_motion = handle_motion;
+
+while (True):
+    sleep(1);
+    #pir.wait_for_motion();
+  #  handle_motion();
+    
